157d6b1fb1cb2556a3a41e2cdd286d090be78b2a
danielmarbach <daniel.marbach@openplace.net>
2019-11-22 10:14:51 +0100

    Introducing better host integration for logging

---
+++ src/NServiceBus.MicrosoftLogging.Hosting/DeferredLoggerFactory.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Concurrent;
+using NServiceBus.Logging;
+
+class DeferredLoggerFactory : ILoggerFactory
+{
+    public ConcurrentDictionary<string, ConcurrentQueue<(LogLevel level, string message)>> deferredLogs = new ConcurrentDictionary<string, ConcurrentQueue<(LogLevel level, string message)>>();
+
+    public DeferredLoggerFactory(LogLevel filterLevel)
+    {
+        this.filterLevel = filterLevel;
+        isDebugEnabled = LogLevel.Debug >= filterLevel;
+        isInfoEnabled = LogLevel.Info >= filterLevel;
+        isWarnEnabled = LogLevel.Warn >= filterLevel;
+        isErrorEnabled = LogLevel.Error >= filterLevel;
+        isFatalEnabled = LogLevel.Fatal >= filterLevel;
+    }
+
+    public ILog GetLogger(Type type)
+    {
+        return GetLogger(type.FullName);
+    }
+
+    public ILog GetLogger(string name)
+    {
+        
+
+        return new NamedLogger(name, this)
+        {
+            IsDebugEnabled = isDebugEnabled,
+            IsInfoEnabled = isInfoEnabled,
+            IsWarnEnabled = isWarnEnabled,
+            IsErrorEnabled = isErrorEnabled,
+            IsFatalEnabled = isFatalEnabled
+        };
+    }
+
+    public void Write(string name, LogLevel messageLevel, string message)
+    {
+        if (messageLevel < filterLevel)
+        {
+            return;
+        }
+        var logQueues = deferredLogs.GetOrAdd(name, new ConcurrentQueue<(LogLevel level, string message)>());
+        logQueues.Enqueue((messageLevel, message));
+    }
+
+    LogLevel filterLevel;
+    bool isDebugEnabled;
+    bool isErrorEnabled;
+    bool isFatalEnabled;
+    bool isInfoEnabled;
+    bool isWarnEnabled;
+}
---
+++ src/NServiceBus.MicrosoftLogging.Hosting/HostBuilderExtensions.cs
@@ -0,0 +1,31 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NServiceBus;
+using NServiceBus.Logging;
+
+/// <summary>
+/// Extension methods to configure NServiceBus Logging for the .NET Core generic host.
+/// </summary>
+public static class HostBuilderExtensions
+{
+    /// <summary>
+    /// Configures the host to use <see cref="MicrosoftLogFactory"/> for NServiceBus logging.
+    /// </summary>
+    /// <param name="hostBuilder"></param>
+    /// <param name="deferLogging">Intercepts all NServiceBus logging that is done before the endpoint is started and forwards them <see cref="MicrosoftLogFactory"/>.</param>
+    /// <remarks>If <paramref name="deferLogging"/> is used this extension method needs to be called before any usage of NServiceBus such as <code>UseNServiceBus</code>.</remarks>
+    public static IHostBuilder UseMicrosoftLogFactoryLogging(this IHostBuilder hostBuilder, bool deferLogging = true)
+    {
+        if (deferLogging)
+        {
+            LogManager.Use<DeferredLoggerFactoryDefinition>();
+        }
+
+        hostBuilder.ConfigureServices((ctx, serviceCollection) =>
+        {
+            serviceCollection.AddHostedService<NServiceBusLoggingHostedService>();
+        });
+
+        return hostBuilder;
+    }
+}
