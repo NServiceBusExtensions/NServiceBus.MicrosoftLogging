12441fa20c49e994934c1d63736ddd45f83563fc
SimonCropp <simon.cropp@gmail.com>
2019-11-26 09:29:53 +1100

    .

--- src/Tests/IntegrationTests.cs
+++ src/Tests/IntegrationTests.cs
@@ -6,7 +6,7 @@ using Xunit;
 using Xunit.Abstractions;
 
 public class IntegrationTests :
-    XunitApprovalBase
+    VerifyBase
 {
     [Fact]
     public async Task Ensure_log_messages_are_redirected()
