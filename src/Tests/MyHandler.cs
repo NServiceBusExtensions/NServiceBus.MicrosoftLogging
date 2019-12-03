a77a3169999a4d5b392ae22e64d1a20f0e315cde
Simon Cropp <simon.cropp@gmail.com>
2019-12-03 13:12:06 +1100

    Update MyHandler.cs

--- src/Tests/MyHandler.cs
+++ src/Tests/MyHandler.cs
@@ -5,11 +5,9 @@ using NServiceBus.Logging;
 class MyHandler :
     IHandleMessages<MyMessage>
 {
-    static ILog log = LogManager.GetLogger<MyHandler>();
-
     public Task Handle(MyMessage message, IMessageHandlerContext context)
     {
-        log.Info($"Hello from MyHandler {message.DateSend}");
+        LogManager.GetLogger<MyHandler>().Info($"Hello from MyHandler {message.DateSend}");
         return Task.FromResult(0);
     }
 }
